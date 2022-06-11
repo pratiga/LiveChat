7fd6315d7d89da3e97eeb8ae66087359394d9022
Pratima <gaitrisanu2468@gmail.com>
2022-06-11 13:34:56 +0545

    display all message
    Signed-off-by: Pratima <gaitrisanu2468@gmail.com>

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 
 function Chat({ socket, username, room }) {
     const [currentMessage, setCurrentMessage] = useState("");
+    const [messageList, setMessageList] = useState([]);
+
     
     const sendMessage =async () => {
         if (currentMessage !== ""){
@@ -19,13 +21,32 @@ function Chat({ socket, username, room }) {
             await socket.emit("send_message", messageData);
         }
     }
+    useEffect(() => {
+        socket.on("receive_message", (data) => {
+            setMessageList((list) => [...list, data]);
+        });
+    }, [socket]);
     return (
-        <div>
+        <div className='chat-window'>
         <div className='chat-header'>
             <p>Live Chat</p>
         </div>
         <div className='chat-body'>
-            
+            {messageList.map((messageContent) => {
+                return(
+                    <div className='message'>
+                        <div>
+                            <div classname="message-content">
+                            <p>{messageContent.message}</p>
+                            </div>
+                            <div classname="message-meta">
+                            <p> {messageContent.time}</p>
+                            <p> {messageContent.time}</p>
+                            </div>
+                        </div>
+                    </div>
+                )
+            })}
         </div>
         <div className='chat-footer'>
            <input type="text"
